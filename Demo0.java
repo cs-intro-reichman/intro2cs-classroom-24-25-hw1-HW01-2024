0aa7fa05955340cece56706d3fb0c10ab2007e7f
YamlRUNI <155106022+YamlRUNI@users.noreply.github.com>
2024-11-24 17:29:43 +0200

    Update Demo0.java

--- Demo0.java
+++ Demo0.java
@@ -1,4 +1,8 @@
-// Prints the numbers 0 to 5.
+//#feedback
+// hey Yael i don't see any implementation here.
+//did something go wrong during solving?
+
+	// Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
 public class PrintTo5 {
 	public static void main(String[] args) {
